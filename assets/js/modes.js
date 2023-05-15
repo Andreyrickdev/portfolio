cfd06e06bdefdaa3a6be44af1960b99745aa14f8
andreyrickdev <contato-andrey@outlook.com>
2023-05-15 19:33:20 -0300

    selected class

--- assets/js/modes.js
+++ assets/js/modes.js
@@ -3,3 +3,4 @@ const toggle = document.getElementById("toggle");
 const darkOrLight = document.getElementById('dark-or-light'); 
 
 const header = document.getElementsByClassName('bg-header')[0];
+const article = document.getElementsByClassName('bg-article')[0];
