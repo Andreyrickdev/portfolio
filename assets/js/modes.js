dd1df1503c5f81a30e87918aeddb424bd779f460
andreyrickdev <contato-andrey@outlook.com>
2023-05-15 19:33:47 -0300

    selected class

--- assets/js/modes.js
+++ assets/js/modes.js
@@ -5,3 +5,4 @@ const darkOrLight = document.getElementById('dark-or-light');
 const header = document.getElementsByClassName('bg-header')[0];
 const article = document.getElementsByClassName('bg-article')[0];
 const projects = document.getElementsByClassName('bg-projects')[0];
+const footer = document.getElementsByClassName('bg-footer')[0];
