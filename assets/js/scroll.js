10a478ddecfb3a71b14bc21e60ca3bc837396852
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:47:01 -0300

    function, prevent default of events, get scroll

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -8,3 +8,10 @@ function getScrollTopByHref (element) {
     const id = element.getAttribute('href');
     return document.querySelector(id).offsetTop;
 }
+
+function scrollToIdOnClick (event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 80;
+
+    scrollToPosition(to);
+}
