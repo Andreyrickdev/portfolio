c57349d69796d4162c834edc824431d4cb701ae7
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:41:30 -0300

    eventListener of click

--- assets/js/projectsOpenAndCloseHamburger.js
+++ assets/js/projectsOpenAndCloseHamburger.js
@@ -1,3 +1,8 @@
 const hamburger = document.getElementsByClassName('dad-hamburger')[0];
 const transparentBox = document.getElementById('box-of-tagsSkills');
 const skillsBox = document.getElementById('tagsSkills');
+
+hamburger.addEventListener('click', () => {
+    transparentBox.classList.toggle('active');
+    skillsBox.classList.toggle('active');
+})
