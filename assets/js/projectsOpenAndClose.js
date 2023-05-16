b86dc0c20fece8ed9b53075f7252a4e35aaaaa58
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:31:47 -0300

    selected class

--- assets/js/projectsOpenAndClose.js
+++ assets/js/projectsOpenAndClose.js
@@ -1,2 +1,3 @@
 const imgOfProject = document.body.getElementsByClassName('project-img');
 const boxInfs = document.body.getElementsByClassName('box-infs');
+const closeIcon = document.body.getElementsByClassName('material-icons');
