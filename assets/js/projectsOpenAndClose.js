4e4cb1af34e50e6636037c60d923fd771b7cf49f
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:32:33 -0300

    loop structure

--- assets/js/projectsOpenAndClose.js
+++ assets/js/projectsOpenAndClose.js
@@ -1,3 +1,21 @@
 const imgOfProject = document.body.getElementsByClassName('project-img');
 const boxInfs = document.body.getElementsByClassName('box-infs');
 const closeIcon = document.body.getElementsByClassName('material-icons');
+
+for (let i = 0; i < imgOfProject.length; i++) {
+    imgOfProject[i].addEventListener('click', showOrHide);
+    closeIcon[i].addEventListener('click', hide);
+
+    function showOrHide () {
+
+        boxInfs[i].classList.toggle('active');
+        closeIcon[i].classList.toggle('active');
+    
+    }
+
+    function hide () {
+
+        boxInfs[i].classList.remove('active');
+        closeIcon[i].classList.remove('active');
+    }
+}
