9701e53968bb81adfcdf93da5ba133dbdd0fae60
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:45:12 -0300

    function, get atribbute href and select id

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,3 +3,8 @@ const menuItems = document.querySelectorAll('#menu li a');
 menuItems.forEach (item => {
     item.addEventListener('click', scrollToIdOnClick);
 });
+
+function getScrollTopByHref (element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
