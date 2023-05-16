31efd2a5d3417f2aa8b50e4df829c289e4ca9088
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:36:06 -0300

    seleted tags

--- assets/js/projectsTagSelect.js
+++ assets/js/projectsTagSelect.js
@@ -1,3 +1,4 @@
 const tags = document.getElementById('tagsSkills');
 const tagsSkills = tags.getElementsByTagName('li');
 const dadAllProjects = document.getElementById('all-projects');
+const allProjects = dadAllProjects.getElementsByTagName('li');
