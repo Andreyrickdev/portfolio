09bbaf6ac6816afa463dd45c269fdf1b40afb4eb
andreyrickdev <contato-andrey@outlook.com>
2023-05-16 04:38:18 -0300

    loop structure

--- assets/js/projectsTagSelect.js
+++ assets/js/projectsTagSelect.js
@@ -6,3 +6,46 @@ const htmlCssTags = document.getElementsByClassName('h-c');
 const jsTags = document.getElementsByClassName('js');
 const transparentBoxx = document.getElementById('box-of-tagsSkills');
 const skillsBoxx = document.getElementById('tagsSkills');
+
+for (let i = 0; i < tagsSkills.length; i++) {
+
+    tagsSkills[i].addEventListener('click', () => {
+
+        if(i === 0) {
+            for (let tags of allProjects) {
+                tags.style.display = 'inline-block';
+            }
+
+            transparentBoxx.classList.remove('active');
+            skillsBoxx.classList.remove('active');
+        }
+
+        if(i === 1) {
+            for(let tags of allProjects) {
+                tags.style.display = 'none';
+            }
+
+            for(let tags of htmlCssTags) {
+                tags.style.display = 'inline-block';
+            }
+
+            transparentBoxx.classList.remove('active');
+            skillsBoxx.classList.remove('active');
+        }
+
+        if(i === 2) {
+            for(let tags of allProjects) {
+                tags.style.display = 'none';
+            }
+
+            for(let tags of jsTags) {
+                tags.style.display = 'inline-block';
+            }
+
+            transparentBoxx.classList.remove('active');
+            skillsBoxx.classList.remove('active');
+        }
+
+    })
+
+}
