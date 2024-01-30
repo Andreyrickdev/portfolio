36897d988d06b6e23c716794e8ce1465f2a8ad0d
Andreyrickdev <contato-andrey@outlook.com>
2024-01-30 19:08:57 -0300

    put all codes in immediate function

--- assets/js/projectsOpenAndCloseHamburger.js
+++ assets/js/projectsOpenAndCloseHamburger.js
@@ -1,12 +1,12 @@
-const hamburger = document.getElementsByClassName('dad-hamburger')[0];
-const transparentBox = document.getElementById('box-of-tagsSkills');
-const skillsBox = document.getElementById('tagsSkills');
-
-hamburger.addEventListener('click', () => {
-    transparentBox.classList.toggle('active');
-    skillsBox.classList.toggle('active');
-})
-
 (function () {
+
+    const hamburger = document.getElementsByClassName('dad-hamburger')[0];
+    const transparentBox = document.getElementById('box-of-tagsSkills');
+    const skillsBox = document.getElementById('tagsSkills');
+    
+    hamburger.addEventListener('click', () => {
+        transparentBox.classList.toggle('active');
+        skillsBox.classList.toggle('active');
+    })
 
 })();
