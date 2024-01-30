abb7d245e8ef6cb0456e183bb95a5379b679fc1b
Andreyrickdev <contato-andrey@outlook.com>
2024-01-30 19:08:18 -0300

    put all codes in immediate function

--- assets/js/projectsTagSelect.js
+++ assets/js/projectsTagSelect.js
@@ -1,55 +1,56 @@
-const tags = document.getElementById('tagsSkills');
-const tagsSkills = tags.getElementsByTagName('li');
-const dadAllProjects = document.getElementById('all-projects');
-const allProjects = dadAllProjects.getElementsByTagName('li');
-const htmlCssTags = document.getElementsByClassName('h-c');
-const jsTags = document.getElementsByClassName('js');
-const transparentBoxx = document.getElementById('box-of-tagsSkills');
-const skillsBoxx = document.getElementById('tagsSkills');
-
-for (let i = 0; i < tagsSkills.length; i++) {
-
-    tagsSkills[i].addEventListener('click', () => {
-
-        if(i === 0) {
-            for (let tags of allProjects) {
-                tags.style.display = 'inline-block';
-            }
-
-            transparentBoxx.classList.remove('active');
-            skillsBoxx.classList.remove('active');
-        }
-
-        if(i === 1) {
-            for(let tags of allProjects) {
-                tags.style.display = 'none';
-            }
-
-            for(let tags of htmlCssTags) {
-                tags.style.display = 'inline-block';
-            }
-
-            transparentBoxx.classList.remove('active');
-            skillsBoxx.classList.remove('active');
-        }
-
-        if(i === 2) {
-            for(let tags of allProjects) {
-                tags.style.display = 'none';
-            }
-
-            for(let tags of jsTags) {
-                tags.style.display = 'inline-block';
-            }
-
-            transparentBoxx.classList.remove('active');
-            skillsBoxx.classList.remove('active');
-        }
-
-    })
-
-}
-
 (function () {
+
+    const tags = document.getElementById('tagsSkills');
+    const tagsSkills = tags.getElementsByTagName('li');
+    const dadAllProjects = document.getElementById('all-projects');
+    const allProjects = dadAllProjects.getElementsByTagName('li');
+    const htmlCssTags = document.getElementsByClassName('h-c');
+    const jsTags = document.getElementsByClassName('js');
+    const transparentBoxx = document.getElementById('box-of-tagsSkills');
+    const skillsBoxx = document.getElementById('tagsSkills');
+    
+    for (let i = 0; i < tagsSkills.length; i++) {
+    
+        tagsSkills[i].addEventListener('click', () => {
+    
+            if(i === 0) {
+                for (let tags of allProjects) {
+                    tags.style.display = 'inline-block';
+                }
+    
+                transparentBoxx.classList.remove('active');
+                skillsBoxx.classList.remove('active');
+            }
+    
+            if(i === 1) {
+                for(let tags of allProjects) {
+                    tags.style.display = 'none';
+                }
+    
+                for(let tags of htmlCssTags) {
+                    tags.style.display = 'inline-block';
+                }
+    
+                transparentBoxx.classList.remove('active');
+                skillsBoxx.classList.remove('active');
+            }
+    
+            if(i === 2) {
+                for(let tags of allProjects) {
+                    tags.style.display = 'none';
+                }
+    
+                for(let tags of jsTags) {
+                    tags.style.display = 'inline-block';
+                }
+    
+                transparentBoxx.classList.remove('active');
+                skillsBoxx.classList.remove('active');
+            }
+    
+        })
+    
+    }
+    
 
 })();
